1797a6509f72b93d6aa3c41d64d8ba51bb0521eb
relational-router-4343 <ajagunnaabiodun752@yahoo.com>
2019-10-09 05:13:27 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -15,3 +15,64 @@ def map_to_square(source_array)
     new_array =source_array.map {|num|  num**2}
     return new_array
 end
+def reduce_to_total(source_array, starting_point = 0)
+    sum = starting_point
+    i = 0 
+    while i < source_array.length
+    sum = sum +  source_array[i] 
+    i+=1
+    end
+    return sum
+end  
+def reduce_to_all_true(source_array)
+  sum = 0
+    i = 0 
+    while i < source_array.length
+    sum = sum +  source_array[i] 
+    if source_array == sum
+     
+    end
+    i+=1
+      return true
+    end
+    
+end
+def reduce_to_all_true(source_array)
+  sum = 0
+    i = 0 
+    while i < source_array.length
+    sum = sum +  source_array[i] 
+    if source_array != sum
+     
+    end
+    i+=1
+      return false
+    end
+    
+end
+def reduce_to_all_true(source_array)
+  sum = 0
+    i = 0 
+    while i < source_array.length
+    sum = sum +  source_array[i] 
+    if source_array == sum
+     
+    end
+    i+=1
+  end
+      return true
+end
+def reduce_to_all_true(source_array)
+  sum = 0
+    i = 0 
+    while i < source_array.length
+    sum = sum +  source_array[i] 
+    if source_array != sum
+     
+    end
+    i+=1
+      return false
+    end
+    
+end
+    
