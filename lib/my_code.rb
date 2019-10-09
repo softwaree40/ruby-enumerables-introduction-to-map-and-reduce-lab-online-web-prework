ed69f3d178ce59cd12bb97c082fcfb654dc86876
relational-router-4343 <ajagunnaabiodun752@yahoo.com>
2019-10-09 01:12:16 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,17 @@
-# My Code here....
+def  map_to_negativize(source_array)
+    new_array =source_array.map {|num|  num * -1}
+    return new_array
+    
+end
+def map_to_no_change(source_array)
+    new_array =source_array.map {|num|  num}
+    return new_array
+end
+def map_to_double(source_array)
+  new_array =source_array.map {|num|  num*2}
+    return new_array
+end
+def map_to_square(source_array)
+    new_array =source_array.map {|num|  num**2}
+    return new_array
+end
