deed4849856f78453a0131d6b031b8f3338b2bc1
relational-router-4343 <ajagunnaabiodun752@yahoo.com>
2019-10-09 16:59:36 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -34,3 +34,13 @@ def reduce_to_all_true(source_array)
     
     return true  
 end
+def reduce_to_all_true(source_array)
+  
+    i = 0 
+    while i < source_array.length
+      return false if source_array[i]
+      i+=1
+    end
+    
+    return true  
+end
